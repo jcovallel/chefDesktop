40ad3e609233cf139d33ceaebfb67f480dbf5b5e
Julio Cesar <jcovallel@unal.edu.co>
2020-05-27 16:12:19 -0500

    desktop app for normal admin

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Main extends Application {
+
+    @FXML
+    private AnchorPane dnd;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("chefDesktop");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
